9bc574223125f2aa7ac244e1057b5f677b80c834
Jerry-Zhu-ICS4U1-02-2024-2025 <jerry.zhu2@student.tdsb.on.ca>
2025-02-19 15:57:17 +0000

    WIP

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,36 @@ public class PracticeProblem {
 
 	}
 
+public static int lettesToFive(String phrase, char c){
+	int counter = 0;
+	for (int i = 0; i < phrase.length(); i++){
+		if (c == phrase.charAt(i)){
+counter++;
+		}
+		if(counter == 5){
+			return i;
+		} 
+	}
+	return -1;
+}
+
+public static int countLetter(String phrase, char c){
+	int counter = 0;
+	for (int i = 0; i < phrase.length(); i++){
+		if (c == phrase.charAt(i)){
+counter++;
+		}
+	}
+	return counter;
+}
+
+public static int oddSum(int num, int num2){
+	int sum = 0;
+	for (int i = Math.min(num , num2); i <= Math.max(num, num2); i++){
+		if (i % 2 != 0){
+			sum += i;
+		}
+	}
+	return sum;
+}
 }
